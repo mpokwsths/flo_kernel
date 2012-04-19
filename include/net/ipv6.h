66d34877b603cc252ca2a038085462772efbb11d
Eric W. Biederman <ebiederm@xmission.com>
2012-04-19 13:45:29 +0000

    net: Delete all remaining instances of ctl_path
    We don't use struct ctl_path anymore so delete the exported constants.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Acked-by: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ipv6.h
+++ include/net/ipv6.h
@@ -114,7 +114,6 @@ struct frag_hdr {
 
 /* sysctls */
 extern int sysctl_mld_max_msf;
-extern struct ctl_path net_ipv6_ctl_path[];
 
 #define _DEVINC(net, statname, modifier, idev, field)			\
 ({									\
