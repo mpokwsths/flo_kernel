49bfcdf1e7712592788a4f548e7a88bd97389c89
Gavin Shan <shangw@linux.vnet.ibm.com>
2012-05-29 15:06:44 -0700

    mm/buddy: dump PG_compound_lock page flag
    The array pageflag_names[] does conversion from page flags into their
    corresponding names so that a meaningful representation of the
    corresponding page flag can be printed.  This mechanism is used while
    dumping page frames.  However, the array missed PG_compound_lock.  So
    the PG_compound_lock page flag would be printed as a digital number
    instead of a meaningful string.
    
    The patch fixes that and prints "compound_lock" for the PG_compound_lock
    page flag.
    
    Signed-off-by: Gavin Shan <shangw@linux.vnet.ibm.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	mm/page_alloc.c

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6092,6 +6092,9 @@ static const struct trace_print_flags pageflag_names[] = {
 #ifdef CONFIG_MEMORY_FAILURE
 	{1UL << PG_hwpoison,		"hwpoison"	},
 #endif
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+	{1UL << PG_compound_lock,	"compound_lock"	},
+#endif
 	{1UL << PG_readahead,           "PG_readahead"  },
 };
 
