e1c8963a565c1eb34adb9d1a67317838b1d3031a
Suman Tatiraju <sumant@codeaurora.org>
2013-11-18 18:31:19 -0800

    msm: kgsl: Add a power constraints interface
    Add an interface to hint the user specified power
    constraint. User can specify a particular GPU power level
    or a bandwidth vote. The power constraints are per context
    or command batch. Power constraints are only for hinting
    and should be used judiciously.
    
    Change-Id: Ic10a5fff4abc88c51cd45a7d7ac58c0d4e5dcb25
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -136,8 +136,8 @@ struct kgsl_functable {
 	void (*drawctxt_destroy) (struct kgsl_context *context);
 	long (*ioctl) (struct kgsl_device_private *dev_priv,
 		unsigned int cmd, void *data);
-	int (*setproperty) (struct kgsl_device *device,
-		enum kgsl_property_type type, void *value,
+	int (*setproperty) (struct kgsl_device_private *dev_priv,
+		enum kgsl_property_type type, void __user *value,
 		unsigned int sizebytes);
 	int (*postmortem_dump) (struct kgsl_device *device, int manual);
 	void (*drawctxt_sched)(struct kgsl_device *device,
@@ -396,6 +396,7 @@ struct kgsl_process_private;
  * @pagefault_ts: global timestamp of the pagefault, if KGSL_CONTEXT_PAGEFAULT
  * is set.
  * @flags: flags from userspace controlling the behavior of this context
+ * @pwr_constraint: power constraint from userspace for this context
  */
 struct kgsl_context {
 	struct kref refcount;
@@ -411,6 +412,7 @@ struct kgsl_context {
 	struct kgsl_event_group events;
 	unsigned int pagefault_ts;
 	unsigned int flags;
+	struct kgsl_pwr_constraint pwr_constraint;
 };
 
 struct kgsl_process_private {
