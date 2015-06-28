e9ece2f0ff04b7bbd7a831034cafe7540a8dd6c9
Kostas K <mpokwsths@gmail.com>
2015-06-28 18:39:13 +0300

    cpufreq: ondemand: Improve efficiency & cpu load calculation
    Squashed myfluxi's commits:
    cpufreq: ondemand: Calculate load_at_max_freq based on current policy->max
    cpufreq: ondemand: Use interruptible wait to not affect load average
    
    Signed-off-by: Kostas K <mpokwsths@gmail.com>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -852,7 +852,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	}
 
 	/* calculate the scaled load across CPU */
-	load_at_max_freq = (cur_load * policy->cur)/policy->cpuinfo.max_freq;
+	load_at_max_freq = (cur_load * policy->cur)/policy->max;
 
 	cpufreq_notify_utilization(policy, load_at_max_freq);
 	/* Check for frequency increase */
@@ -1038,7 +1038,7 @@ static int dbs_sync_thread(void *data)
 	this_dbs_info = &per_cpu(od_cpu_dbs_info, cpu);
 
 	while (1) {
-		wait_event(this_dbs_info->sync_wq,
+		wait_event_interruptible(this_dbs_info->sync_wq,
 			   sync_pending(this_dbs_info) ||
 			   kthread_should_stop());
 
